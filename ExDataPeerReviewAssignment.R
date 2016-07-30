fac19153cb6523d2e55870abec3d2963674710c1
robmill <r_miller213@hotmail.com>
2016-07-30 06:45:39 -0700

    First Draft Script

---
+++ ExDataPeerReviewAssignment.R
@@ -0,0 +1,17 @@
+###################################### 
+# ExData Peer Review Assignment
+# Rob Miller
+#
+######################################
+
+
+# Read data
+
+NEI<-readRDS("summarySCC_PM25.rds")
+SCC<-readRDS("Source_Classification_Code.rds")
+
+yearVec<-unique(NEI$year)
+for(i in 1:yearVec){
+        
+}
+        
